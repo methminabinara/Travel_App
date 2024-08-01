28ca79bba3bacdc7fcaa22d23169f537f0af683f
Methmina Binara <binaraepa@gmail.com>
2024-08-01 13:28:50 +0530

    chore: Created constants

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,15 @@
+import Image from "next/image"
 import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <nav className="border-2 border-red-500 flex-between max-container padding-container relative z-30 py-5">
+    <nav className="flex-between max-container padding-container relative z-30 py-5">
       <Link href="/">
-        Home
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29}/>
+
+        <ul className="hidden h-full gap-12 lg:flex">
+
+        </ul>
       </Link>
     </nav>
   )
