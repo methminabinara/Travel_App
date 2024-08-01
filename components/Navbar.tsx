7e80a54a5840039a2b9e644f992305fc8fd8f9c1
Methmina Binara <binaraepa@gmail.com>
2024-08-01 13:14:32 +0530

    chore: Added assets

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link"
 
 const Navbar = () => {
   return (
     <nav className="border-2 border-red-500 flex-between max-container padding-container relative z-30 py-5">
-
+      <Link href="/">
+        Home
+      </Link>
     </nav>
   )
 }
